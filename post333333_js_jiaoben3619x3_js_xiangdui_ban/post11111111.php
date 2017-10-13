4c229d1afe53082b8f7973c42b320e1f69c09f02
miaoer0028 <32697065+miaoer0028@users.noreply.github.com>
2017-10-13 11:11:22 -0500

    Initial commit

---
+++ post333333_js_jiaoben3619x3_js_xiangdui_ban/post11111111.php
@@ -0,0 +1,278 @@
+
+
+<h3></>
+
+</form>
+
+<div    style=color:#FF0000>
+
+
+
+<form    action="post11111111.php"   method="post">
+ 
+
+请选择后区号码最少选2个++：<br><hr>
+
+
+
+<table border="2">
+<tr>
+<td>
+
+
+
+<td>
+<td>
+
+
+<h3   style=color:#FF0000>
+
+<input type="checkbox" name="0['0']" value="0">0<br />
+
+<td>
+<td>
+
+
+<h3   style=color:#FF0000>
+
+<input type="checkbox" name="1['1']" value="1">1<br />
+
+<td>
+
+
+<h3   style=color:#FF0000>
+
+<input type="checkbox" name="2['2']"
+value="2">2<br>
+
+<td>
+<td>
+
+
+<h3   style=color:#FF0000>
+
+<input type="checkbox" name="3['3']" value="3">3<br />
+
+<td>
+<td>
+
+<h3   style=color:#FF0000>
+
+<input type="checkbox" name="4['4']" value="4">4<br />
+
+<td>
+<td>
+
+<h3   style=color:#FF0000>
+
+<input type="checkbox" name="5['5']" value="5">5<br />
+
+<td>
+<td>
+
+
+<h3   style=color:#FF0000>
+
+<input type="checkbox" name="6['6']" value="6">6<br />
+
+<td/>
+<td>
+
+
+
+<h3   style=color:#FF0000>
+
+<input type="checkbox" name="7['7']" value="7">7<br />
+
+<td/>
+<td>
+
+
+
+<h3   style=color:#FF0000>
+
+
+<input type="checkbox" name="8['8']" value="8">8<br/>
+
+<td>
+
+
+<tr>
+
+<td>
+
+<td>
+
+<h3   style=color:#FF0000>
+
+<input type="checkbox" name="9['9']" value="9">9<br />
+<td>
+
+
+<td>
+<td>
+
+
+<h3   style=color:#FF0000>
+
+<input type="checkbox" name="10['10']" value="10">10<br />
+
+<td>
+<td>
+
+
+<h3   style=color:#FF0000>
+
+<input type="checkbox" name="11['11']" value="11">11<br />
+
+<td>
+<td>
+
+
+<h3   style=color:#FF0000>
+
+<input type="checkbox" name="12['12']" value="12">12<br />
+
+<td>
+
+<td>
+
+
+
+<h3   style=color:#FF0000>
+<input type="checkbox" name="13['13']" value="13">13<br />
+<td>
+<td>
+
+
+<h3   style=color:#FF0000>
+<input type="checkbox" name="14['14']" value="14">14<br />
+<td>
+<td>
+
+
+<td>
+<td>
+
+
+<h3   style=color:#FF0000>
+
+<input type="checkbox" name="15['15']" value="15">15<br />
+
+<td>
+<td>
+
+
+
+<td>
+<td>
+
+
+<h3   style=color:#FF0000>
+
+<input type="checkbox" name="16['16']" value="16">16<br />
+
+<td>
+
+
+</div>
+
+
+<br/>
+
+
+<table border="2">
+<tr>
+<td>
+
+
+<h3   style=color:#FF0000>
+
+
+
+
+<input     style="font-size:40px"    type="submit"   value="开始选号" >
+
+
+
+<br/>
+
+
+</form>
+
+<h4>两行三列后区：</h4>
+<table border="1">
+<tr>
+  <td>
+
+
+<?php
+
+echo "<pre>";
+
+
+echo "<center>";
+
+
+echo "<h2  style=color:#FF0000  >双色球POST勾选随机版后区</>";
+
+
+echo "<h2  style=color:#FF0000 >您勾选的号码是</><td>";
+
+
+
+print_r($_POST);   //打印出勾选号码
+
+echo "<h2 style=color:#FF0000 >您勾选后随机的号码是</><br/>";
+
+
+print_r(array_rand($_POST,1));  //在勾选号码中随机
+
+
+echo "<br/>";
+
+print_r(array_rand($_POST,1));  //在勾选号码中随机 
+
+
+
+?>
+
+
+<td/>
+
+</tr>
+</table>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ post333333_js_jiaoben3619x3_js_xiangdui_ban/post66666666.php
@@ -0,0 +1,556 @@
+
+<center>
+
+
+
+<h3   style=color:#FF0000>
+
+
+
+<form    action="post66666666.php" method="post">
+ 
+
+<table border="2">
+<tr>
+<td>
+
+<h3   style=color:#FF0000>
+
+
+
+请选择前区号码最少选5个##：<br><hr>
+
+<table border="2">
+<tr>
+
+
+<td>
+
+<td>
+
+<td>
+
+<td>
+
+<h3   style=color:#FF0000>
+
+
+<input type="checkbox" name="1['1']" value="1">1<br />
+<td/>
+<td>
+
+
+
+<h3   style=color:#FF0000>
+
+
+<input type="checkbox" name="2['2']"
+value="2">2<br>
+
+<td/>
+
+<td>
+
+<h3   style=color:#FF0000>
+
+
+<input type="checkbox" name="3['3']" value="3">3<br />
+
+<td/>
+<td>
+
+<h3   style=color:#FF0000>
+
+
+<input type="checkbox" name="4['4']" value="4">4<br />
+<td/>
+
+<td>
+
+
+<h3   style=color:#FF0000>
+
+
+<input type="checkbox" name="5['5']" value="5">5<br />
+
+
+
+
+
+<td/>
+
+
+<td>
+
+
+<h3   style=color:#FF0000>
+
+
+<input type="checkbox" name="6['6']" value="6">6<br />
+
+<td/>
+
+<td>
+
+
+
+<h3   style=color:#FF0000>
+
+<input type="checkbox" name="7['7']" value="7">7<br />
+<td/>
+<td>
+
+
+
+<h3   style=color:#FF0000>
+
+
+<input type="checkbox" name="8['8']" value="8">8<br />
+
+<td/>
+
+<td>
+
+<tr/>
+
+<tr>
+
+
+<td>
+
+<input type="checkbox" name="0['0']" value="0">0<br />
+
+<input type="checkbox" name="0['0']" value="0">0<br />
+<td/>
+
+<td>
+
+
+
+
+<tr/>
+
+
+<tr>
+
+
+
+<input type="checkbox" name="0['0']" value="0">0<br />
+
+<input type="checkbox" name="0['0']" value="0">0<br />
+<td/>
+
+
+
+<td>
+
+<tb>
+
+
+<td>
+
+
+
+<h3   style=color:#FF0000>
+
+<input type="checkbox" name="9['9']" value="9">9<br />
+
+<td/>
+<td>
+
+
+
+<h3   style=color:#FF0000>
+
+<input type="checkbox" name="10['10']" value="10">10<br />
+
+<td/>
+
+<td>
+
+
+<h3   style=color:#FF0000>
+
+<input type="checkbox" name="11['11']" value="11">11<br />
+
+<td/>
+
+
+<td>
+
+
+<h3   style=color:#FF0000>
+
+<input type="checkbox" name="12['12']" value="12">12<br />
+
+
+<td/>
+
+<td>
+
+
+
+
+<h3   style=color:#FF0000>
+
+
+<input type="checkbox" name="13['13']" value="13">13<br />
+
+
+<td/>
+
+<td>
+
+
+<h3   style=color:#FF0000>
+
+<input type="checkbox" name="14['14']" value="14">14<br />
+
+<td/>
+
+<td>
+
+
+<h3   style=color:#FF0000>
+
+
+<input type="checkbox" name="15['15']" value="15">15<br />
+
+
+<td/>
+
+<td>
+
+
+
+<h3   style=color:#FF0000>
+
+<input type="checkbox" name="16['16']" value="16">16<br />
+
+
+<td/>
+
+
+<td>
+
+<tr/>
+
+
+<tr>
+
+
+<td>
+
+<input type="checkbox" name="0['0']" value="0">0<br />
+
+<input type="checkbox" name="0['0']" value="0">0<br />
+<td/>
+
+
+<td>
+
+<h3   style=color:#FF0000>
+
+<input type="checkbox" name="17['17']" value="17">17<br />
+
+<td/>
+
+
+<td>
+
+
+<h3   style=color:#FF0000>
+
+<input type="checkbox" name="18['18']" value="18">18<br />
+
+
+<td/>
+
+<td>
+
+
+<h3   style=color:#FF0000>
+
+<input type="checkbox" name="19['19']" value="19">19<br />
+
+<td/>
+
+<td>
+
+
+<h3   style=color:#FF0000>
+
+<input type="checkbox" name="20['20']" value="20">20<br />
+
+
+<td/>
+
+<td>
+
+
+<h3   style=color:#FF0000>
+
+<input type="checkbox" name="21['21']" value="21">21<br />
+
+
+<td/>
+
+
+<td>
+
+
+<h3   style=color:#FF0000>
+
+
+<input type="checkbox" name="22['22']" value="22">22<br />
+
+<td/>
+
+
+<td>
+
+
+<h3   style=color:#FF0000>
+
+<input type="checkbox" name="23['23']" value="23">23<br />
+
+
+<td/>
+
+<td>
+
+<h3   style=color:#FF0000>
+
+<input type="checkbox" name="24['24']" value="24">24<br />
+
+<td/>
+
+<tr/>
+
+<tr>
+
+<td>
+
+<input type="checkbox" name="0['0']" value="0">0<br />
+
+<input type="checkbox" name="0['0']" value="0">0<br />
+<td/>
+
+
+<td>
+
+<h3   style=color:#FF0000>
+
+<input type="checkbox" name="25['25']" value="25">25<br />
+
+
+<td/>
+<td>
+
+
+<h3   style=color:#FF0000>
+
+
+<input type="checkbox" name="26['26']" value="26">26<br />
+
+
+<td/>
+<td>
+
+
+<h3   style=color:#FF0000>
+
+<input type="checkbox" name="27['27']" value="27">27<br />
+
+
+<td/>
+
+<td>
+
+
+<h3   style=color:#FF0000>
+
+<input type="checkbox" name="28['28']" value="28">28<br />
+
+<td/>
+
+<td>
+
+<h3   style=color:#FF0000>
+
+<input type="checkbox" name="29['29']" value="29">29<br />
+
+<td/>
+
+<td>
+
+<h3   style=color:#FF0000>
+
+
+<input type="checkbox" name="30['30']" value="30">30<br />
+
+<td/>
+
+<td>
+
+
+<h3   style=color:#FF0000>
+
+<input type="checkbox" name="31['31']" value="31">31<br />
+
+<td/>
+
+<tr/>
+
+<tr>
+
+
+<td>
+
+<input type="checkbox" name="0['0']" value="0">0<br />
+
+<input type="checkbox" name="0['0']" value="0">0<br />
+<td/>
+
+<td>
+
+
+<h3   style=color:#FF0000>
+
+<input type="checkbox" name="32['32']" value="32">32<br />
+
+<td/>
+
+<td>
+
+<h3   style=color:#FF0000>
+
+
+<input type="checkbox" name="33['33']" value="33">33<br />
+
+<td/>
+
+<td>
+
+
+<h3   style=color:#FF0000>
+
+<input type="checkbox" name="34['34']" value="34">34<br />
+
+
+<td/>
+<td>
+
+
+<h3   style=color:#FF0000>
+
+<input type="checkbox" name="35['35']" value="35">35<br />
+
+
+<td/>
+<td>
+
+<input type="checkbox" name="0['0']" value="0">0<br />
+
+<input type="checkbox" name="0['0']" value="0">0<br />
+<td/>
+
+<tr/>
+
+
+
+<table/>
+
+<br>
+
+<hr>
+
+
+
+<input     style="font-size:40px"    type="submit"   value="开始选号" >
+
+
+
+
+</form>
+
+<div    style=color:#FF0000>
+
+
+<h4>两行三列前区：</h4>
+<table border="1">
+<tr>
+  <td>
+
+
+<?php
+echo "<pre>";
+echo "<center>";
+echo "<h2  style=color:#FF0000  >双色球POST勾选随机版前区</>";
+
+echo "<h2  style=color:#FF0000 >您勾选的号码是</><td>";
+
+
+
+
+print_r ($_POST);   //打印出勾选号码
+
+
+echo "<h2 style=color:#FF0000 >您勾选后随机的号码是</><br/>";
+
+
+print_r(array_rand($_POST,6));   //在勾选号码中随机
+
+echo "<br/>";
+
+print_r(array_rand($_POST,6));   //在勾选号码中随机
+
+
+?>
+<td/>
+
+</tr>
+
+</table>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
